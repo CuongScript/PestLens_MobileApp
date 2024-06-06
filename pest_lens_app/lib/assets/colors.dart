21e1023d91866f7afe11323494ff879abebc879f
Minh Phan <minh40490@gmail.com>
2024-06-06 11:23:47 +0700

    update/login_screen
    update background color

---
+++ pest_lens_app/lib/assets/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const Color primaryBackgroundColor = Color.fromRGBO(227, 236, 254, 0);
--- pest_lens_app/lib/pages/login_page.dart
+++ pest_lens_app/lib/pages/login_page.dart
@@ -3,5 +3,10 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
-  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(227, 236, 254, 0),
+    );
+  }  
 }
