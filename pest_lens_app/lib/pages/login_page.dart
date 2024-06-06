952d49c4650eb477ae480c37810ba83bcffa9ea8
Minh Phan <minh40490@gmail.com>
2024-06-06 11:50:07 +0700

    update/login_page
    simple setup

--- pest_lens_app/lib/pages/login_page.dart
+++ pest_lens_app/lib/pages/login_page.dart
@@ -7,10 +7,50 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: primaryBackgroundColor,
-      body: Column(
-        children: [],
-      ),
-    );
+        backgroundColor: primaryBackgroundColor, // Use the color constant here
+        appBar: AppBar(
+          title: const Text('Primary Background Color Example'),
+        ),
+        body: const Center(
+          child: Text('Hello, World!'),
+        ),
+      );
+    // return Scaffold(
+    //   backgroundColor: primaryBackgroundColor,
+    //   body: SafeArea(
+    //     child: Center(
+    //       child: Column(
+    //         children: const [
+    //           const SizedBox(
+    //             height: 50,
+    //           ),
+    //           // Logo
+    //           const Icon(
+    //             Icons.lock,
+    //             size: 100,
+    //           ),
+
+    //           const SizedBox(
+    //             height: 50,
+    //           ),
+
+    //           // App name
+
+    //           // Username textfield
+
+    //           // Password textfield
+
+    //           // Forgot password
+
+    //           // Sign in button
+
+    //           // Sign up button
+
+    //           // Viet+Eng icon button
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
