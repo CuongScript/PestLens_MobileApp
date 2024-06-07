a37f8171143fad2d079d8739c3a6458c2f738eb3
Minh Phan <minh40490@gmail.com>
2024-06-07 13:08:17 +0700

    feat/splash_screen

--- pest_lens_app/lib/main.dart
+++ pest_lens_app/lib/main.dart
@@ -1,6 +1,52 @@
+// import 'package:flutter/material.dart';
+// import 'package:pest_lens_app/l10n/l10n.dart';
+// import 'package:pest_lens_app/pages/login_page.dart';
+// import 'package:flutter_localizations/flutter_localizations.dart';
+// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   Locale? _locale;
+
+//   void setLocale(Locale locale) {
+//     setState(() {
+//       _locale = locale;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       locale: _locale,
+//       supportedLocales: L10n.all,
+//       localizationsDelegates: const [
+//         AppLocalizations.delegate,
+//         GlobalMaterialLocalizations.delegate,
+//         GlobalWidgetsLocalizations.delegate,
+//         GlobalCupertinoLocalizations.delegate,
+//       ],
+//       home: LoginPage(
+//         onLocaleChange: setLocale,
+//       ),
+//     );
+//   }
+// }
+
+// main.dart
 import 'package:flutter/material.dart';
 import 'package:pest_lens_app/l10n/l10n.dart';
-import 'package:pest_lens_app/pages/login_page.dart';
+import 'package:pest_lens_app/pages/splash_screen.dart'; // Import your splash screen
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -8,27 +54,13 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Locale? _locale;
-
-  void setLocale(Locale locale) {
-    setState(() {
-      _locale = locale;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      locale: _locale,
       supportedLocales: L10n.all,
       localizationsDelegates: const [
         AppLocalizations.delegate,
@@ -36,9 +68,7 @@ class _MyAppState extends State<MyApp> {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      home: LoginPage(
-        onLocaleChange: setLocale,
-      ),
+      home: const SplashScreen(), // Set the home to SplashScreen
     );
   }
 }
---
+++ pest_lens_app/lib/pages/splash_screen.dart
@@ -0,0 +1,53 @@
+// import 'package:animated_splash_screen/animated_splash_screen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:lottie/lottie.dart';
+// import 'package:page_transition/page_transition.dart';
+// import 'package:pest_lens_app/pages/login_page.dart';
+
+// class SplashScreen extends StatelessWidget {
+//   const SplashScreen({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return AnimatedSplashScreen(
+//       splash: Lottie.asset(
+//         'lib/assets/splash_animation/splash_animation.json',
+//         repeat: false,
+//       ),
+//       nextScreen: LoginPage(),
+//       splashIconSize: 250,
+//       duration: 3500,
+//       splashTransition: SplashTransition.fadeTransition,
+//       pageTransitionType: PageTransitionType.leftToRight,
+//     );
+//   }
+// }
+
+// splash_screen.dart
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:pest_lens_app/assets/colors.dart';
+import 'package:pest_lens_app/components/locale_handler.dart'; // Import the locale handler
+
+class SplashScreen extends StatelessWidget {
+  // const SplashScreen({Key? key}) : super(key: key);
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Lottie.asset(
+        'lib/assets/splash_animation/splash_animation.json',
+        repeat: true,
+      ),
+      nextScreen: const LocaleHandler(), // Set the next screen to LocaleHandler
+      splashIconSize: 250,
+      duration: 3500,
+      splashTransition: SplashTransition.fadeTransition,
+      pageTransitionType: PageTransitionType.leftToRight,
+      backgroundColor: primaryBackgroundColor,
+    );
+  }
+}
