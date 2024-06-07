9e0d630e921b7ea37574345332179313b4c049da
Minh Phan <minh40490@gmail.com>
2024-06-07 14:51:17 +0700

    feat/splash_screen

--- pest_lens_app/lib/main.dart
+++ pest_lens_app/lib/main.dart
@@ -1,37 +1,67 @@
+// import 'package:flutter/material.dart';
+// import 'package:pest_lens_app/pages/admin_main_page.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   Locale? _locale;
+
+//   void setLocale(Locale locale) {
+//     setState(() {
+//       _locale = locale;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: const AdminMainPage(),
+//       theme: ThemeData(
+//         dividerTheme: const DividerThemeData(
+//           thickness: 2,
+//           color: Colors.black,
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
-import 'package:pest_lens_app/pages/admin_main_page.dart';
+import 'package:pest_lens_app/l10n/l10n.dart';
+import 'package:pest_lens_app/pages/splash_screen.dart'; // Import your splash screen
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Locale? _locale;
-
-  void setLocale(Locale locale) {
-    setState(() {
-      _locale = locale;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const AdminMainPage(),
-      theme: ThemeData(
-        dividerTheme: const DividerThemeData(
-          thickness: 2,
-          color: Colors.black,
-        ),
-      ),
+      supportedLocales: L10n.all,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      home: const SplashScreen(), // Set the home to SplashScreen
     );
   }
 }
