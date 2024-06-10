cabd28a7eaa2630268f48f0745242f715d863aea
Minh Phan <minh40490@gmail.com>
2024-06-10 13:12:45 +0700

    feat/reset-password-screen

--- pest_lens_app/lib/pages/reset_password.dart
+++ pest_lens_app/lib/pages/reset_password.dart
@@ -1,45 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:pest_lens_app/assets/colors.dart';
+import 'package:pest_lens_app/components/my_back_button.dart';
+import 'package:pest_lens_app/components/my_text_style.dart';
+import 'package:pest_lens_app/components/my_text_field.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:pest_lens_app/components/my_submit_button.dart';
+import 'package:pest_lens_app/locale_handler.dart';
 
 class ResetPassword extends StatelessWidget {
   ResetPassword({super.key});
-  final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController = TextEditingController();
+
+  final passController = TextEditingController();
+  final rePassController = TextEditingController();
+
+  void resetPass() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Reset password')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: 'New password',
-                border: OutlineInputBorder(),
+      backgroundColor: primaryBackgroundColor,
+      appBar: AppBar(
+        backgroundColor: primaryBackgroundColor,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: IconButton(
+            icon: const MyBackButton(),
+            onPressed: () {
+              Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context) => const LocaleHandler()),
+                (Route<dynamic> route) =>
+                    false, // This predicate removes all routes
+              );
+            },
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 64,
               ),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _confirmPasswordController,
-              decoration: InputDecoration(
-                labelText: 'Confirm new password',
-                border: OutlineInputBorder(),
+              Text(
+                AppLocalizations.of(context)!.resetPass,
+                style: CustomTextStyles.appName,
               ),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // TODO: Implement reset password logic
-                Navigator.popUntil(context, (route) => route.isFirst);
-              },
-              child: Text('Reset password'),
-            ),
-          ],
+              const SizedBox(
+                height: 9,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                child: Text(
+                  AppLocalizations.of(context)!.textResetPass,
+                  style: CustomTextStyles.forgotPass,
+                ),
+              ),
+              const SizedBox(
+                height: 49,
+              ),
+              MyTextField(
+                controller: passController,
+                obscureText: true,
+                prefixIcon: const Icon(Icons.lock, color: Colors.black),
+                hintText: AppLocalizations.of(context)!.logInPass,
+                showRevealButton: true,
+                textInputAction: TextInputAction
+                    .next, //TextInputAction.done to stop and hide keyboard
+                labelText: AppLocalizations.of(context)!.logInPass,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              MyTextField(
+                controller: rePassController,
+                obscureText: true,
+                prefixIcon: const Icon(Icons.lock, color: Colors.black),
+                hintText: AppLocalizations.of(context)!.reEnterPass,
+                showRevealButton: true,
+                textInputAction: TextInputAction
+                    .next, //TextInputAction.done to stop and hide keyboard
+                labelText: AppLocalizations.of(context)!.reEnterPass,
+              ),
+              const SizedBox(
+                height: 33,
+              ),
+              MySubmitButton(
+                onTap: () {
+                  resetPass;
+                  Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(
+                        builder: (context) => const LocaleHandler()),
+                    (Route<dynamic> route) =>
+                        false, // This predicate removes all routes
+                  );
+                },
+                buttonText: AppLocalizations.of(context)!.sendCode,
+              ),
+            ],
+          ),
         ),
       ),
     );
