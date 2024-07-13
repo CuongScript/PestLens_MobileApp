13bedac3f18332ac2f94da88d18745034dfd0162
CuongNguyen-rmit <s3891758@rmit.edu.vn>
2024-07-13 22:44:14 +0700

    complete signup function

--- pest_lens_app/lib/pages/authen/sign_up_page.dart
+++ pest_lens_app/lib/pages/authen/sign_up_page.dart
@@ -5,9 +5,8 @@ import 'package:pest_lens_app/components/my_text_form_field.dart';
 import 'package:pest_lens_app/components/my_text_style.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:pest_lens_app/components/my_submit_button.dart';
-import 'package:pest_lens_app/locale_handler.dart';
-import 'package:pest_lens_app/pages/authen/login_page.dart';
 import 'package:pest_lens_app/utils/textfield_validator.dart';
+import 'package:pest_lens_app/locale_handler.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:pest_lens_app/utils/config.dart';
@@ -55,15 +54,10 @@ class SignUpPageState extends State<SignUpPage> {
               success
                   ? ElevatedButton(
                       onPressed: () {
-                        Navigator.pop(context);
                         Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => LoginPage(
-                              onLocaleChange: LocaleHandler.setLocale,
-                            ),
-                          ),
-                        );
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const LocaleHandler()));
                       },
                       child: Text(AppLocalizations.of(context)!.comeToLogin),
                     )
