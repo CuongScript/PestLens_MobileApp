b0ce2f3f97e85b5f46a1902219656c116e50bf4c
CuongNguyen-rmit <80303805+CuongNguyen-rmit@users.noreply.github.com>
2024-08-08 16:25:14 +0700

    complete custom style of bottom navbar in farmer page

--- pest_lens_app/lib/pages/farmer/farmer_tab_page.dart
+++ pest_lens_app/lib/pages/farmer/farmer_tab_page.dart
@@ -7,55 +7,108 @@ class FarmerTabPage extends StatefulWidget {
   const FarmerTabPage({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    return _FarmerTabPageState();
-  }
+  State<FarmerTabPage> createState() => _FarmerTabPageState();
 }
 
 class _FarmerTabPageState extends State<FarmerTabPage> {
-  int _selectedPageIndex = 0;
+  int _selectedIndex = 0;
 
-  void _selectPage(int index) {
-    setState(() {
-      _selectedPageIndex = index;
-    });
+  static const List<Widget> _widgetOptions = <Widget>[
+    FarmerMainPage(),
+    Text(
+      "Insect Information Page",
+      style: TextStyle(
+        fontSize: 40,
+      ),
+    ),
+    InsectLookupPage(),
+    Text(
+      "Notification Page",
+      style: TextStyle(
+        fontSize: 40,
+      ),
+    ),
+    SettingsPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    if (index != 2) {
+      // 2 is reserved for FAB
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+  }
+
+  Widget _buildNavBarItem(IconData icon, int index) {
+    return IconButton(
+      icon: Icon(
+        icon,
+        color: _selectedIndex == index ? const Color(0xFF0064c3) : Colors.grey,
+        size: 30,
+      ),
+      onPressed: () => _onItemTapped(index),
+    );
+  }
+
+  List<Widget> _buildBottomNavBarItems() {
+    return [
+      _buildNavBarItem(Icons.home_filled, 0),
+      _buildNavBarItem(Icons.search_outlined, 1),
+      const SizedBox(width: 40), // Space for FAB
+      _buildNavBarItem(Icons.notifications_none_rounded, 3),
+      _buildNavBarItem(Icons.settings_outlined, 4),
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget activePage = const FarmerMainPage();
-
-    if (_selectedPageIndex == 1) {
-      activePage = const Center(
-        child: InsectLookupPage(),
-      );
-    } else if (_selectedPageIndex == 2) {
-      activePage = const Center(child: SettingsPage());
-    }
-
     return Scaffold(
-      body: activePage,
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: _selectPage,
-        currentIndex: _selectedPageIndex,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '',
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      floatingActionButton: SizedBox(
+        width: 70,
+        height: 70,
+        child: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _selectedIndex = 2; // Navigate to InsectLookupPage
+            });
+          },
+          backgroundColor: const Color(0xFF0064c3),
+          shape: const CircleBorder(),
+          elevation: 4,
+          child: const Icon(
+            Icons.camera_alt_outlined,
+            size: 32,
+            color: Colors.white,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.upload_sharp,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 8,
+              offset: const Offset(0, -4),
             ),
-            label: '',
+          ],
+        ),
+        child: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 12,
+          child: SizedBox(
+            height: 80,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: _buildBottomNavBarItems(),
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.settings,
-            ),
-            label: '',
-          )
-        ],
+        ),
       ),
     );
   }
--- pest_lens_app/lib/utils/config.dart
+++ pest_lens_app/lib/utils/config.dart
@@ -1,3 +1,3 @@
 class Config {
-  static const String apiUrl = 'http://192.168.68.59:8818';
+  static const String apiUrl = 'http://192.168.68.69:8818';
 }
