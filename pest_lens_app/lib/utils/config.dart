dcfc6d615ef5b67c6625c7dca68e75a3658c4440
CuongNguyen-rmit <s3891758@rmit.edu.vn>
2024-08-11 22:49:53 +0700

    complete notification feature

--- pest_lens_app/lib/utils/config.dart
+++ pest_lens_app/lib/utils/config.dart
@@ -1,4 +1,4 @@
 class Config {
-  static const String apiUrl = 'http://192.168.68.59:8818';
-  static const String mlAPIUrl = 'http://192.168.68.59:8000';
+  static const String apiUrl = 'http://192.168.1.14:8818';
+  static const String mlAPIUrl = 'http://192.168.1.14:8000';
 }
