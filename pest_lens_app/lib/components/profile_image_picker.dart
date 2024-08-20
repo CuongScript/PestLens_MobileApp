646dd4e1a079929171c890c469bc5ae8251b8c66
CuongNguyen-rmit <s3891758@rmit.edu.vn>
2024-08-21 01:30:00 +0700

    finish get user profile image from aws s3

--- pest_lens_app/lib/components/profile_image_picker.dart
+++ pest_lens_app/lib/components/profile_image_picker.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:pest_lens_app/assets/colors.dart';
+import 'package:pest_lens_app/services/s3_service.dart';
 
 class ProfileImagePicker extends StatefulWidget {
   final String? imageUrl;
@@ -9,49 +11,18 @@ class ProfileImagePicker extends StatefulWidget {
   final Function(File)? onImagePicked;
 
   const ProfileImagePicker({
-    super.key,
+    Key? key,
     this.imageUrl,
     this.isReadOnly = false,
     this.onImagePicked,
-  });
+  }) : super(key: key);
 
   @override
   State<ProfileImagePicker> createState() => _ProfileImagePickerState();
 }
 
 class _ProfileImagePickerState extends State<ProfileImagePicker> {
-  ImageProvider? _image;
   File? _imageFile;
-
-  @override
-  void initState() {
-    super.initState();
-    _setImage(widget.imageUrl);
-  }
-
-  void _setImage(String? url) {
-    if (url != null && url.isNotEmpty) {
-      _image = NetworkImage(url);
-    } else {
-      _image =
-          const AssetImage('lib/assets/images/placeholder_profile_image.png');
-    }
-  }
-
-  void _pickImage() async {
-    if (widget.isReadOnly) return;
-
-    final picker = ImagePicker();
-    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
-
-    if (pickedFile != null) {
-      setState(() {
-        _imageFile = File(pickedFile.path);
-        _image = FileImage(_imageFile!);
-      });
-      widget.onImagePicked?.call(_imageFile!);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,9 +31,12 @@ class _ProfileImagePickerState extends State<ProfileImagePicker> {
       child: Stack(
         alignment: Alignment.bottomRight,
         children: [
-          CircleAvatar(
-            radius: 60,
-            backgroundImage: _image,
+          ClipOval(
+            child: SizedBox(
+              width: 120,
+              height: 120,
+              child: _buildImageContent(),
+            ),
           ),
           if (!widget.isReadOnly)
             Container(
@@ -77,5 +51,73 @@ class _ProfileImagePickerState extends State<ProfileImagePicker> {
         ],
       ),
     );
+  }
+
+  Widget _buildImageContent() {
+    if (_imageFile != null) {
+      return Image.file(
+        _imageFile!,
+        fit: BoxFit.cover,
+      );
+    }
+
+    if (widget.imageUrl == null || widget.imageUrl!.isEmpty) {
+      return _buildPlaceholderImage();
+    }
+
+    if (Uri.tryParse(widget.imageUrl!)?.hasScheme ?? false) {
+      return _buildNetworkImage(widget.imageUrl!);
+    } else {
+      return _buildS3Image(widget.imageUrl!);
+    }
+  }
+
+  Widget _buildPlaceholderImage() {
+    return Image.asset(
+      'lib/assets/images/placeholder_profile_image.png',
+      fit: BoxFit.cover,
+    );
+  }
+
+  Widget _buildNetworkImage(String url) {
+    return FadeInImage.assetNetwork(
+      placeholder: 'lib/assets/images/placeholder_profile_image.png',
+      image: url,
+      fit: BoxFit.cover,
+      imageErrorBuilder: (context, error, stackTrace) {
+        return _buildPlaceholderImage();
+      },
+    );
+  }
+
+  Widget _buildS3Image(String objectKey) {
+    return FutureBuilder<Uint8List>(
+      future: S3Service().getUserProfileImageData(objectKey),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done &&
+            snapshot.hasData) {
+          return Image.memory(
+            snapshot.data!,
+            fit: BoxFit.cover,
+          );
+        } else if (snapshot.hasError) {
+          return _buildPlaceholderImage();
+        } else {
+          return _buildPlaceholderImage();
+        }
+      },
+    );
+  }
+
+  void _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+      widget.onImagePicked?.call(_imageFile!);
+    }
   }
 }
