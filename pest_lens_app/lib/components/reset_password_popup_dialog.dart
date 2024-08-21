fc854104d86669ddbf9c141a44beda9098e36e1b
Minh Phan <minh40490@gmail.com>
2024-08-21 13:14:17 +0700

    feat: implement reset password popup

---
+++ pest_lens_app/lib/components/reset_password_popup_dialog.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:pest_lens_app/components/my_text_style.dart';
+import 'package:pest_lens_app/pages/authen/login_page.dart';
+
+// This function shows a success or failure dialog for password reset
+void showResetPasswordPopup(BuildContext context, bool success) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,  // Prevent dialog from closing on tap outside
+    builder: (BuildContext dialogContext) {
+      return AlertDialog(
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              success ? Icons.check_circle : Icons.error,
+              color: success ? Colors.green : Colors.red,
+              size: 60,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              success ? "Password Reset Successfully" : "Password Reset Failed",
+              textAlign: TextAlign.center,
+              style: CustomTextStyles.labelTextField,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                if (success) {
+                  // Navigate to login page if reset was successful
+                  Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                    (Route<dynamic> route) => false,
+                  );
+                } else {
+                  // Dismiss the dialog and allow the user to try again
+                  Navigator.pop(dialogContext);
+                }
+              },
+              child: Text(success ? "Go to Login" : "Try Again"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: success ? Colors.green : Colors.red, // Button color
+                foregroundColor: Colors.white, // Text color
+              ),
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
